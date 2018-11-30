bb8bb4387d94cec1120ccce5eb81f4fdade4134f
Fernando Ortiz Rico Celio <fernando_oc96@hotmail.ocm>
2018-11-29 21:43:31 -0600

    Thermometer working correctly.

--- js/main.js
+++ js/main.js
@@ -7,3 +7,14 @@ $(document).on('click', '.cardSmall' , function(){
     $(this).toggleClass('cardExtended');
     $(this).toggleClass('cardSmall');
 });
+
+$(document).on('input', '.temp', function() {
+    $('#tempNum').html( $(this).val() +"ºC");
+    if ($(this).val() < 36) {
+      $('#tempDesc').html("Temperatura baja");
+    } else if ($(this).val() > 38) {
+      $('#tempDesc').html("Temperatura alta");
+    } else {
+      $('#tempDesc').html("Temperatura normal");
+    }
+});
