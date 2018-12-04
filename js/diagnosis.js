061872fd93f75c06936c996e4779473395089853
Enrique Aguilar <enriqueao96@gmail.com>
2018-12-03 23:31:08 -0600

    show more diagnostics if have the same probability

--- js/diagnosis.js
+++ js/diagnosis.js
@@ -2,25 +2,33 @@ $(document).ready(function() {
   $(window).on('load', function() {
     var result = JSON.parse(localStorage.getItem('result'));
     var possibleDisease = result[0];
-    var disease = enfermedades[possibleDisease.id];
+    let psoibleScoreH = possibleDisease.total;
+    let card = "";
+    var diseases = result.filter(disease => disease.total === psoibleScoreH);
     var total = 0;
     result.forEach(disease => {
       total += disease.total;
     });
-    var probability = Math.round((possibleDisease.total * 100) / total);
 
-    var card = `<div class="diagnosis open">
-            <div class="header">
-              <div class="left">
-                <h2>${disease.name}</h2>
-                <p>${probability}% de probabilidad</p>
-              </div>
-            </div>
-            <h3>¿Qué es?</h3>
-            <p>${disease.desc}</p>
-            <h3>Tratamiento</h3>
-            <p>${disease.rec}</p>
-          </div>`;
+    diseases.forEach(function (disease) {
+      let probability = Math.round((disease.total * 100) / total);
+      let rec = '';
+      enfermedades[disease.id].rec.forEach(function (recommendation) {
+        rec += `- ${recommendation} <br/>`;
+      });
+      card += `<div class="diagnosis open">
+                  <div class="header">
+                    <div class="left">
+                      <h2>${enfermedades[disease.id].name}</h2>
+                      <p>${probability}% de probabilidad</p>
+                    </div>
+                  </div>
+                  <h3>¿Qué es?</h3>
+                  <p>${enfermedades[disease.id].desc}</p>
+                  <h3>Tratamiento</h3>
+                  <p>${rec}</p>
+                </div>`;
+    });
 
     $('div#inner')
       .empty()
